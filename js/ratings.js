096bd5c6edfbc11653426692f9ac90c4278db45c
ScurtuAdrian <adrianmadalinscurtu@gmail.com>
2023-02-06 12:21:23 +0200

    First commit

---
+++ js/ratings.js
@@ -0,0 +1,15 @@
+function collect_ratings(){
+  let  ratings = {
+    count : "0" ,
+    sum : "0" , 
+    average : "0" 
+  }
+  let reting= 0;
+
+};
+
+const elements =document.querySelectorAll('.rating');
+
+array.forEach(element => {
+    replace(element.id.star) ='';
+});
